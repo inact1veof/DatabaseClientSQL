c4a720ed92ff63c456ff1ddd61295e8675ec5120
inact1veof <54815085+inact1veof@users.noreply.github.com>
2020-12-09 14:40:58 +0500

    addSQLConnect

---
+++ DatabaseClientSQL/DatabaseClientSQL/ConnectToDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DatabaseClientSQL
+{
+    class ConnectToDatabase
+    {
+        SqlConnection database;
+
+        void Connect() //коннект к базе данных (fixme), не  хватает отбора данных, поодключается только к текущей локальной бд
+        {
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Игорь\Desktop\DatabaseClientSQL\DatabaseClientSQL\DatabaseClientSQL\MainDatabase.mdf; Integrated Security = True;";
+            database = new SqlConnection(connectionString);
+
+        }
+    }
+}
